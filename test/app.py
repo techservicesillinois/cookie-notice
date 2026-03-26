732e798a3134e28f3982e5e599a5fb9cde6b0a83
Zach Carrington <zdc@illinois.edu>
2026-03-26 16:02:59 -0500

    Add Jinja templating

--- test/app.py
+++ test/app.py
@@ -4,9 +4,29 @@ from flask import Flask, render_template
 
 WEB_PATH = Path(__file__).resolve().parent.parent / "src" / "web"
 TEMPLATE_FOLDER = WEB_PATH
-app = Flask(__name__, template_folder=str(TEMPLATE_FOLDER), static_url_path="/", static_folder=str(WEB_PATH))
+app = Flask(
+    __name__,
+    template_folder=str(TEMPLATE_FOLDER),
+    static_url_path="/",
+    static_folder=str(WEB_PATH),
+)
 
 
 @app.route("/")
 def stupid_hack():
     return render_template("index.html")
+
+
+@app.route("/uic")
+def stupid_hack_uic():
+    return render_template("uic.html")
+
+
+@app.route("/uis")
+def stupid_hack_uis():
+    return render_template("uis.html")
+
+
+@app.route("/csp")
+def stupid_hack_csp():
+    return render_template("csp.html")
