c4f5962123baf525b02519f9619ecd33bc0277a5
Zach Carrington <zdc@illinois.edu>
2026-03-26 15:09:44 -0500

    template layout

--- test/app.py
+++ test/app.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, render_template
 
-WEB_PATH=Path(__file__).resolve().parent.parent / "src" / "web"
-app = Flask(__name__, static_url_path="/", static_folder=str(WEB_PATH))
+WEB_PATH = Path(__file__).resolve().parent.parent / "src" / "web"
+TEMPLATE_FOLDER = WEB_PATH
+app = Flask(__name__, template_folder=str(TEMPLATE_FOLDER), static_url_path="/", static_folder=str(WEB_PATH))
+
 
 @app.route("/")
 def stupid_hack():
-    return '<meta http-equiv="refresh" content="0; url=/index.html" />'
+    return render_template("index.html")
