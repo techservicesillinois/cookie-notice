9783db6ccd96ffe5c5258a313942a62248768b8e
Zach Carrington <zdc@illinois.edu>
2026-07-20 11:45:08 -0500

    Add vite and nunjucks config

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import nunjucksPlugin from 'vite-plugin-nunjucks'
+import nunjucks from 'nunjucks'
+import { resolve } from 'path'
+
+const templatesDir = resolve(__dirname, 'src/web')
+
+const env = new nunjucks.Environment(
+  new nunjucks.FileSystemLoader(templatesDir),
+  { noCache: true }
+)
+
+export default defineConfig({
+  root: 'src/web',
+  plugins: [nunjucksPlugin({
+    templatesDir,
+    nunjucksEnvironment: env
+  })],
+  build: {
+    rollupOptions: {
+      input: {
+        main:   resolve(__dirname, 'src/web/index.html'),
+        uic:    resolve(__dirname, 'src/web/uic.html'),
+        uis:    resolve(__dirname, 'src/web/uis.html'),
+        csp:    resolve(__dirname, 'src/web/csp.html'),
+        gtag:   resolve(__dirname, 'src/web/gtag.html'),
+        legacy: resolve(__dirname, 'src/web/legacy.html'),
+      }
+    }
+  }
+})
