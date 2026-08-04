0b02ae3500c83d4f5d86b41f871b824760d4aa4d
Olivia Shan <oliviashan2612@gmail.com>
2026-08-04 16:56:21 -0400

    wip adding test configs

---
+++ test/cookie.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { render } from "vitest-browser-lit";
+import { html } from "lit";
+
+const content = html`
+    <ilw-org-chart>
+    </ilw-org-chart>`;
+
+test("renders slotted content", async () => {
+    const screen = render(content);
+    const element = screen.getByText("No organization data provided");
+    await expect.element(element).toBeInTheDocument();
+});
