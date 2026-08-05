3d9c6c4b28d3643888fb1c54734facd18662656e
Tyler Turner <tzturner@illinois.edu>
2026-08-05 15:55:31 -0500

    Axe accessibility test works

---
+++ vite.build.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: "src",
+    build: {
+        outDir: "../dist/cdn",
+        lib: {
+            name: "cookie-notice",
+            entry: "cookie-notice.ts",
+            fileName: "cookie-notice",
+            formats: ["es"],
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: () => {
+                    return "[name][extname]";
+                },
+            },
+        },
+    },
+    server: {
+        hmr: false,
+    },
+});
